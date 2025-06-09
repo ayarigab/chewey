04dae1447687f56ffd5f7fd414f1a8390bf7208f
Naaba Technologies LLC <jessybright66@gmail.com>
2025-06-09 02:07:59 +0000

    Create constants.dart

---
+++ lib/const/constants.dart
@@ -0,0 +1,39 @@
+// App Descriptions
+
+// Naaba Store Keys are provided by Naaba Technologies
+// DO NOT DELETE THESE KEYS, they are essential for the app to function properly
+// and are used to verify the app with Naaba Technologies.
+// If you need to change them, please contact Naaba Technologies for new keys.
+// Or contact the app developer for assistance in changing them or setting up your own keys.
+//  THIS IS A FREE APP AS LONG AS YOU DO NOT DELETE THE KEYS
+String naaKey =
+    "ck_6800281b214cdd2af15faa259277e78f266faac5"; // DONOT DELETE THIS KEY
+String naaCret =
+    "cs_77585c7468ffec136d8c648afe0f55fb8754ca36"; // DONOT DELETE THIS SECRET
+String naaUrl = "https://apps.naabatechs.com"; // DONOT DELETE THIS URL
+String appId = '1590795778'; // DONOT DELETE THIS APP ID
+
+// Decapital HR New Keys
+String cKey = "your wordpress consumer key";
+String cCret = "your wordpress consumer secret";
+
+// EmailJS Keys
+String pubKey = "your emailjs public key";
+String priKey = "your emailjs private key";
+String servID = "your emailjs service id";
+String tempID = "your emailjs template id";
+
+String appStoreId = 'your app store id';
+String appStoreIdWeb = 'your app store id web';
+String playStoreId = 'your play store id';
+String url = "the website url or api url";
+String email = "your email address";
+
+String baseAPI = "/wp-json/wc/v3";
+String baseAPI2 = "/wp-json/wc/v2";
+String authAPI = "/wc-api/v3";
+String authAPI2 = "/wc-api/v2";
+String curSymbol = '₵';
+String currPrefix = 'GH';
+String currency = 'GH₵';
+String currPostFix = '.00';
